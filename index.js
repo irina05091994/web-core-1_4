e86e94fac63b2696f5dcf85baf4968e3d3c26f85
Irina Grudkina <irina05091994@mail.ru>
2025-08-05 16:19:52 +0300

    правки по 1.6

--- index.js
+++ index.js
@@ -12,22 +12,10 @@ function swiperCard() {
         direction: 'horizontal',
         loop: true,
         breakpoints: {
-              320: {
-                  slidesPerView: 1.3,
-                  spaceBetween: 20
-                  },
-              400: {
-                  slidesPerView: 2,
-                  spaceBetween: 90
-                  },
-              500: {
-                   slidesPerView: 2.5,
-                  spaceBetween: 120
-                  },
-              600: {
-                    slidesPerView: 3,
-                    spaceBetween: 140
-                    },
+              320: { slidesPerView: 1.3, spaceBetween: 0},
+              400: {slidesPerView: 2,spaceBetween: 90},
+              500: {slidesPerView: 2.5,spaceBetween: 120},
+              600: {slidesPerView: 3,spaceBetween: 140},
           },
         slideToClickedSlides: true,
         pagination: {
@@ -56,7 +44,6 @@ swiperCard(); // Инициализация свайпера
 }
 });
 
-
 function swiperCardDestroy() {
 
   if (window.innerWidth >= 768 ) {
@@ -74,6 +61,8 @@ window.addEventListener("resize", swiperCard);
 swiperCardDestroy();
 window.addEventListener("resize", swiperCardDestroy);
 
+
+
 console.log('Ширина окна:', window.innerWidth); // Для отладки
 
 // Бургер меню
@@ -85,23 +74,47 @@ const body = document.querySelector('.body-flow');
 
 buttonOpen.addEventListener('click', () => {
 burgerMenu.classList.remove('burger-menu--hide');
-body.classList.toggle("burger-menu--blur");
+body.classList.add("burger-menu--blur");
 });
 
 buttonClose.addEventListener('click', () => {
 burgerMenu.classList.add('burger-menu--hide');
+body.classList.remove("burger-menu--blur");
 });
 
+// Кнопка показать скрыть блок Repair
+const buttonRepair = document.getElementById('showButtonTwo');
+const appRepair = document.querySelector('.section-repair__show-conteiner--hidden');
+const buttonImgRepair = document.querySelector('.button-learn-more__img-repair');
+const buttonTextRepair = document.querySelector('.button-learn-more__text-repair');
+
+let Open = false; // Флаг для отслеживания состояния блока
+
+buttonRepair.addEventListener('click', () => {
+  appRepair.style.transition = "all 0.6s ease-in-out";
+  appRepair.classList.toggle("section-repair__show-conteiner");
+  buttonImg.classList.toggle("button-learn-more__img--rotate");
+  
+
+  if (!Open) {
+    buttonTextRepair.textContent = "Скрыть";
+  } else{
+    buttonTextRepair.textContent = "Показать все";
+  }
+
+  Open = !Open; // Переключаем состояние
+});
+ 
 // Кнопка показать скрыть
 const button = document.querySelector('.button-learn-more--show');
 const app = document.querySelector('.show-conteiner--hidden');
 const buttonImg = document.querySelector('.button-learn-more__img');
 const buttonText = document.querySelector('.button-learn-more__text1');
-const buttonText2 = document.querySelector('.button-learn-more__text2--hide');
+
 
 let isOpen = false; // Флаг для отслеживания состояния блока
 
-button.addEventListener('click', () => {
+  button.addEventListener('click', () => {
   app.style.transition = "all 0.6s ease-in-out";
   app.classList.toggle("show-conteiner");
   button.classList.toggle("button-learn-more--hide");
@@ -116,6 +129,55 @@ button.addEventListener('click', () => {
   isOpen = !isOpen; // Переключаем состояние
 });
 
+// Обратная связь
 
+const buttonOpenFeed = document.getElementById('buttonOpenFeed');
+const buttonOpenFeedMob = document.getElementById('buttonOpenFeedMob');
+const buttonCloseFeed = document.getElementById('buttonCloseFeed');
+const feedback = document.querySelector('.feedback-con');
+const feedbackBlur = document.querySelector('.feedback--blur--hide');
+
+
+buttonOpenFeedMob.addEventListener('click', () => {
+feedback.classList.remove('feedback--hide');
+feedbackBlur.classList.add("feedback--blur");
+}); 
+
+buttonOpenFeed.addEventListener('click', () => {
+feedback.classList.remove('feedback--hide');
+feedbackBlur.classList.add("feedback--blur");
+
+});
+
+buttonCloseFeed.addEventListener('click', () => {
+feedback.classList.add('feedback--hide');
+feedbackBlur.classList.remove("feedback--blur");
+});
+
+
+// Заказать звонок
+
+const buttonOpenCall = document.getElementById('buttonOpenCall');
+const buttonOpenCallMob = document.getElementById('buttonOpenCallMob');
+const buttonCloseCall = document.getElementById('buttonCloseCall');
+const feedCall = document.querySelector('.feedback-call');
+
+
+
+buttonOpenCallMob.addEventListener('click', () => {
+feedCall.classList.remove('feedback--hide');
+feedbackBlur.classList.add("feedback--blur");
+}); 
+
+buttonOpenCall.addEventListener('click', () => {
+feedCall.classList.remove('feedback--hide');
+feedbackBlur.classList.add("feedback--blur");
+
+});
+
+buttonCloseCall.addEventListener('click', () => {
+feedCall.classList.add('feedback--hide');
+feedbackBlur.classList.remove("feedback--blur");
+});
 
 
